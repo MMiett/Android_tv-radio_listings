a8b0644e3719e9c3d6bb0cb9397fb1a3ba7f088e
Mikko Miettinen <mmiett200@caledonian.ac.uk>
2013-02-25 12:40:01 +0000

    string fixes

--- TVListngs/gen/org/MMiett/tvlistings/R.java
+++ TVListngs/gen/org/MMiett/tvlistings/R.java
@@ -33,6 +33,8 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int next_day=0x7f040004;
+        public static final int prev_day=0x7f040003;
     }
     public static final class style {
         /** 
