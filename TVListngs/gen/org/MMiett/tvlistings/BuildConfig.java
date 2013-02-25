5cd5d8e248995f440b6294f3d632620bdacfd9d8
Mikko Miettinen <mmiett200@caledonian.ac.uk>
2013-02-25 04:53:12 +0000

    Firstcommit

--- TVListngs/gen/org/MMiett/tvlistings/BuildConfig.java
+++ TVListngs/gen/org/MMiett/tvlistings/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package org.MMiett.tvandradiolistings;
+package org.MMiett.tvlistings;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- TVListngs/gen/org/MMiett/tvlistings/R.java
+++ TVListngs/gen/org/MMiett/tvlistings/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package org.MMiett.tvandradiolistings;
+package org.MMiett.tvlistings;
 
 public final class R {
     public static final class attr {
@@ -14,13 +14,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int ChannelSpinner=0x7f070001;
+        public static final int ListingContext=0x7f070005;
+        public static final int MainContainer=0x7f070000;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070004;
+        public static final int menu_settings=0x7f070007;
+        public static final int temporalTV=0x7f070006;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
